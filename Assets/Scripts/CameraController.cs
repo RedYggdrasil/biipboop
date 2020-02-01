22cd0e63bdce5a60c07e21f35abf7ef43e280434
SmartGoat38 <35306962+SmartGoat38@users.noreply.github.com>
2020-02-01 20:01:13 +0100

    finalized cam system

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -18,16 +18,19 @@ public class CameraController : MonoBehaviour
 
     private Vector2 downLeftCorner;
     private Vector2 upRightCorner;
+    private Vector3 offset;
 
     void Start() 
     {
         cam = Camera.main;
         camCenter = new Vector2(cam.pixelWidth * 0.5f, cam.pixelHeight * 0.5f);
 
-        downLeftCorner = new Vector2(camCenter.x*midRectWidth, camCenter.y*midRectHeight);
+        downLeftCorner = new Vector2(camCenter.x*(1-midRectWidth), camCenter.y*(1-midRectHeight));
         upRightCorner = new Vector2(camCenter.x*(1+midRectWidth), camCenter.y*(1+midRectHeight));
         print(downLeftCorner);
         print(upRightCorner);
+        offset = targetAgent.position - transform.position;
+        offset.y = 0;
     }
 
     // Update is called once per frame
@@ -35,9 +38,16 @@ public class CameraController : MonoBehaviour
     {
         Vector3 agentPos = cam.WorldToScreenPoint(targetAgent.position);
         agentPos = new Vector2(agentPos.x, agentPos.y);
-        if (agentPos.x < downLeftCorner.x || agentPos.x > upRightCorner.x || agentPos.y < downLeftCorner.y || agentPos.y > upRightCorner.y) 
+        Vector3 dist = targetAgent.position - transform.position - offset;
+        dist.y = 0;
+        dist = dist.normalized;
+        if (agentPos.x < downLeftCorner.x || agentPos.x > upRightCorner.x)
         {
-            //transform.Translate(Space.World)
+            transform.Translate(0f, 0f, dist.z*Time.deltaTime*speed,  Space.World);
+        }
+        if (agentPos.y < downLeftCorner.y || agentPos.y > upRightCorner.y) 
+        {
+            transform.Translate(dist.x*Time.deltaTime*speed, 0f, 0f,  Space.World);
         }
     }
 }
