e5f08c7a968dd5755ef11f35e45a501fb8fe6120
SmartGoat38 <35306962+SmartGoat38@users.noreply.github.com>
2020-02-01 18:22:42 +0100

    avancement cam 3

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -16,17 +16,28 @@ public class CameraController : MonoBehaviour
 
     private Camera cam;
 
+    private Vector2 downLeftCorner;
+    private Vector2 upRightCorner;
+
     void Start() 
     {
         cam = Camera.main;
-        camCenter = new Vector2(cam.pixelWidth, cam.pixelHeight);
+        camCenter = new Vector2(cam.pixelWidth * 0.5f, cam.pixelHeight * 0.5f);
+
+        downLeftCorner = new Vector2(camCenter.x*midRectWidth, camCenter.y*midRectHeight);
+        upRightCorner = new Vector2(camCenter.x*(1+midRectWidth), camCenter.y*(1+midRectHeight));
+        print(downLeftCorner);
+        print(upRightCorner);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 agentPos = cam.WorldToScreenPoint(targetAgent.position);
-        //Debug.LogWarning(agentPos);
-        //if (agentPos.x)
+        agentPos = new Vector2(agentPos.x, agentPos.y);
+        if (agentPos.x < downLeftCorner.x || agentPos.x > upRightCorner.x || agentPos.y < downLeftCorner.y || agentPos.y > upRightCorner.y) 
+        {
+            //transform.Translate(Space.World)
+        }
     }
 }
