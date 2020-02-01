1c94211e190b9be4790b59ecf4af533637219359
SmartGoat38 <35306962+SmartGoat38@users.noreply.github.com>
2020-02-01 23:11:20 +0100

    a

---
+++ Assets/Scripts/PrefManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class PrefManager 
+{
+    [HideInInspector]
+    public static bool musicToggled = true;
+    [HideInInspector]
+    public static float soundVolume = 0.5f;
+}
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Image musicToggleButton;
+    public Sprite onMusicSprite;
+    public Sprite offMusicSprite;
+
+    public void OnMusicToggle() 
+    {
+        if (!PrefManager.musicToggled) 
+        {
+            PrefManager.musicToggled = true;
+            musicToggleButton.sprite = onMusicSprite;
+        } 
+        else 
+        {
+            PrefManager.musicToggled = false;
+            musicToggleButton.sprite = offMusicSprite;
+        }
+    }
+
+    public void OnVolumeChange(float newValue)  
+    {
+        PrefManager.soundVolume = newValue;
+    }
+
+}
