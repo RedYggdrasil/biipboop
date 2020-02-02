cfa48cba6cb054f61a02a47a416ebc176dadb269
RedYggdrasil <quentin.noguier@yahoo.fr>
2020-02-02 15:43:25 +0100

    Working Quit Button

---
+++ Assets/QuitApp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+	public void QuitAppFunction()
+	{
+#if UNITY_ENGINE
+
+#else
+		Application.Quit();
+#endif
+	}
+}
