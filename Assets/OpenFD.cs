1130a90e1fdbc0db1d371177e73d58ede08f1fe1
JulienB <julien.brulle@gmail.com>
2020-02-02 10:09:55 +0100

    les fenêtreeeeees

---
+++ Assets/OpenFD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenFD : MonoBehaviour
+{
+    Animator anim;
+    bool open;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        open = false;
+    }
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (open == false)
+            {
+                anim.SetTrigger("Open");
+                open = true;
+            }
+            else if (open == true)
+            {
+                anim.SetTrigger("Close");
+                open = false;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/OnMouseOverColorDroite.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseOverColorDroite : MonoBehaviour
+{
+    private Color m_MouseOverColor = Color.red;
+    private Color m_OriginalColor;
+    private MeshRenderer m_Renderer;
+    public GameObject LaPorte;
+
+    void Start()
+    {
+        m_Renderer = GetComponent<MeshRenderer>();
+        m_OriginalColor = m_Renderer.material.color;
+    }
+
+    void OnMouseOver()
+    {
+        m_Renderer.material.color = m_MouseOverColor;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (LaPorte.transform.rotation.eulerAngles.y == 270)
+            {
+                LaPorte.transform.Rotate(0, 90, 0);
+            }
+            else
+            {
+                LaPorte.transform.Rotate(0, -90, 0);
+            }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        m_Renderer.material.color = m_OriginalColor;
+    }
+}
